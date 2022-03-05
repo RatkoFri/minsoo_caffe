273665c561fecfe0297dbd69e2af4038d6f72b88
RatkoFRI <Ratko.Pilipovic@fri.uni-lj.si>
2022-03-05 08:36:58 +0100

    ILM2 fix

--- src/caffe/util/math_functions.cu
+++ src/caffe/util/math_functions.cu
@@ -360,8 +360,8 @@ void caffe_gpu_gemm_approxV2<float>(const CBLAS_TRANSPOSE TransA,
         (dop_B, dop_A, C, N, M, K, DRUM_K,
         ALLNUMBITS, FRACBITS,  alpha, beta);    
       break;
-    case 3: // ILM1
-      mult_bfloat16_ILM1<<<blocksPerGrid,threadsPerBlock>>>
+    case 3: // ILM2
+      mult_bfloat16_ILM2<<<blocksPerGrid,threadsPerBlock>>>
         (dop_B, dop_A, C, N, M, K, DRUM_K,
         ALLNUMBITS, FRACBITS,  alpha, beta);   
       break;
