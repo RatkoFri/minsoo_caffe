ef7d21eba1e4b72af4ab76a9f0992df111dcefc6
RatkoFri <ratko.pilipovic@fri.uni-lj.si>
2022-11-17 13:58:45 +0100

    playing with ILM12

--- src/caffe/layers/conv_approx_layer.cu
+++ src/caffe/layers/conv_approx_layer.cu
@@ -541,7 +541,7 @@ void ConvolutionApproxLayer<Dtype>::Backward_gpu(const vector<Blob<Dtype>*>& top
     if (this->bias_term_ && this->param_propagate_down_[1]) {
       Dtype* bias_diff = this->blobs_[1]->mutable_gpu_diff();
       for (int n = 0; n < this->num_; ++n) {
-        this->backward_gpu_bias(bias_diff, top_diff + n * this->top_dim_);
+        this->backward_gpu_bias_approx(bias_diff, top_diff + n * this->top_dim_);
       }
     }
     if (this->param_propagate_down_[0] || propagate_down[i]) {
